0cfeb5012a447033966885582c057b4913a7c314
kevinlynx <zmhn320@163.com>
2012-02-22 16:36:41 +0800

    replace hirefire with workless

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,8 @@ Profile::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.after_initialize do
+    Delayed::Job.scaler = :heroku_cedar
+  end
 end
