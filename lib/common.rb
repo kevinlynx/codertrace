08d6e035a7f6a2c266b4b68ab4a47c2f6be8afca
kevinlynx <zmhn320@163.com>
2012-02-27 21:08:12 +0800

    .

--- lib/common.rb
+++ lib/common.rb
@@ -1,7 +1,6 @@
 class Common
   def self.force_utf8!(s)
-    #s.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
-    #s.encode!('UTF-8', 'UTF-16')
-    s.encode!('UTF-8', 'UTF-8', :invalid => :replace)
+    s.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
+    s.encode!('UTF-8', 'UTF-16')
   end
 end
