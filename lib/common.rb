2881c623b8109cffccaf28674c7f3bd154403b71
kevinlynx <zmhn320@163.com>
2012-02-27 18:17:06 +0800

    because of ConverterNotFound error

--- lib/common.rb
+++ lib/common.rb
@@ -1,6 +1,7 @@
 class Common
   def self.force_utf8!(s)
-    s.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
-    s.encode!('UTF-8', 'UTF-16')
+    #s.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
+    #s.encode!('UTF-8', 'UTF-16')
+    s.encode!('UTF-8', 'UTF-8', :invalid => :replace)
   end
 end
