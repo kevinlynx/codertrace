a3f45f567d24b5d606388fd7bdbf3c1d685228c1
kevinlynx <zmhn320@163.com>
2012-02-27 23:07:26 +0800

    update

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  def history_content
+    content = ""
+    File.open('public/history.txt', 'r') do |file|
+        content = file.sysread(file.size)
+        Common.force_utf8! content
+        content = Maruku.new(content).to_html
+    end
+    content
+  end
 end
